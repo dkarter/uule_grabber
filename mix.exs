e9b17476d171a121a82541a45195e7e73375f079
Dorian Karter <dkarter@gmail.com>
2016-07-31 22:17:49 -0500

    Fix package definition in mix.exs for hex

--- mix.exs
+++ mix.exs
@@ -2,20 +2,16 @@ defmodule UuleGrabber.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :uule_grabber,
-     version: "0.1.0",
-     elixir: "~> 1.3",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     description: "Generates uule codes for google to allow localized searches.",
-     deps: deps()]
-  end
-
-  def package do
     [
-      maintainers: ["Dorian Karter"],
-      licenses: ["MIT"],
-      links: %{"Github" => "https://github.com/dkarter/uule_grabber"}
+      app: :uule_grabber,
+      name: "UULE Grabber",
+      version: "0.1.0",
+      elixir: "~> 1.3",
+      package: package,
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      description: "Generates uule codes for google to allow localized searches.",
+      deps: deps()
     ]
   end
 
@@ -25,5 +21,13 @@ defmodule UuleGrabber.Mixfile do
 
   defp deps do
     []
+  end
+
+  defp package do
+    [
+      maintainers: ["Dorian Karter"],
+      licenses: ["MIT"],
+      links: %{"Github" => "https://github.com/dkarter/uule_grabber"}
+    ]
   end
 end
