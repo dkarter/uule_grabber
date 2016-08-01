5054c1b697b2ad2240408b3c337c71b76c22cdc9
Dorian Karter <dkarter@gmail.com>
2016-07-31 21:50:47 -0500

    Add package information for hex

--- mix.exs
+++ mix.exs
@@ -7,7 +7,16 @@ defmodule UuleGrabber.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     description: "Generates uule codes for google to allow localized searches."
      deps: deps()]
+  end
+
+  def package do
+    [
+      maintainers: "Dorian Karter",
+      license: "MIT",
+      links: %{"Github" => "https://github.com/dkarter/uule_grabber"}
+    ]
   end
 
   def application do
