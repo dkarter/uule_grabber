e67cde12bd0927b464437e99dd3c7ea3223cc36c
Dorian Karter <dkarter@gmail.com>
2016-07-31 22:02:32 -0500

    Fix syntax error in mix.exs

--- mix.exs
+++ mix.exs
@@ -7,14 +7,14 @@ defmodule UuleGrabber.Mixfile do
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     description: "Generates uule codes for google to allow localized searches."
+     description: "Generates uule codes for google to allow localized searches.",
      deps: deps()]
   end
 
   def package do
     [
-      maintainers: "Dorian Karter",
-      license: "MIT",
+      maintainers: ["Dorian Karter"],
+      licenses: ["MIT"],
       links: %{"Github" => "https://github.com/dkarter/uule_grabber"}
     ]
   end
