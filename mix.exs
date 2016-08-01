ebdb65b37cb38d40a8516eb248544719999611ea
Dorian Karter <dkarter@gmail.com>
2016-07-31 21:22:34 -0500

    Remove uneeded csv parser dependency

--- mix.exs
+++ mix.exs
@@ -10,23 +10,11 @@ defmodule UuleGrabber.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :csv]]
+    [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    [{:csv, "~> 1.4.2"}]
+    []
   end
 end
